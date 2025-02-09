6e74e3be577b65d1725a339ba438456a46a99468
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-02-09 19:51:09 +0800

    完成mysql的驱动测试

---
+++ TestDB/src/com/msb/test01/Test.java
@@ -0,0 +1,34 @@
+package com.msb.test01;
+
+import java.sql.*;
+
+public class Test {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        System.out.println(System.getProperty("java.class.path"));
+
+        // 加载 MySQL 驱动（可选）
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // 获取链接
+        String url="jdbc:mysql://127.0.0.1:3306/msbDB?useSSL=false&useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai&allowPublicKeyRetrieval=true";
+
+        String username = "root";
+
+        String password = "";
+
+        Connection con = DriverManager.getConnection(url, username, password);
+
+        //创建会话
+        Statement stmt = con.createStatement();
+
+        //发送sql请求
+        ResultSet rs = stmt.executeQuery("select * from books");
+
+        //处理结果
+        while (rs.next()) {
+            System.out.println("图书编号 " + rs.getInt("id")+ " 图书名称" + rs.getString("book") + " 图书作者" + rs.getString("author"));
+        }
+
+    }
+}
